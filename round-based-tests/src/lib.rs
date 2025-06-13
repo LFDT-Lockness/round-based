2501d4e92ad5d12f663c8143c912cec9e9f0f3ba
Denis Varlakov <denis@dfns.co>
2025-06-13 10:24:02 +0200

    Add more comments
    Signed-off-by: Denis Varlakov <denis@dfns.co>

--- round-based-tests/src/lib.rs
+++ round-based-tests/src/lib.rs
@@ -5,6 +5,7 @@ use round_based::{state_machine::ProceedResult, Incoming, Outgoing};
 /// methods for output assertions like `output.expect_eq()`
 pub struct PartySim<S>(S);
 
+/// Wraps a state machine and returns [`PartySim`]
 pub fn new_one_party_sim<'a, M, F>(
     protocol: impl FnOnce(round_based::state_machine::MpcParty<M>) -> F,
 ) -> PartySim<impl round_based::state_machine::StateMachine<Msg = M, Output = F::Output> + 'a>
@@ -16,6 +17,10 @@ where
 }
 
 impl<S: round_based::state_machine::StateMachine> PartySim<S> {
+    /// Feeds an incoming message to the state machine
+    ///
+    /// State machine **must be** in the state waiting for the incoming message. If state
+    /// machine is in other state (e.g. wants to send a message), this function will panic.
     #[track_caller]
     pub fn receives(&mut self, msg: Incoming<S::Msg>) {
         loop {
@@ -38,6 +43,10 @@ impl<S: round_based::state_machine::StateMachine> PartySim<S> {
             .expect("couldn't feed a message into simulation")
     }
 
+    /// Retrieves an outgoing message from the state machine
+    ///
+    /// State machine **must be** in the state of sending a message. If state machine is in
+    /// other state (e.g. waits for an incoming message), this function will panic.
     #[track_caller]
     pub fn sends(&mut self) -> Expect<Outgoing<S::Msg>> {
         loop {
@@ -49,6 +58,10 @@ impl<S: round_based::state_machine::StateMachine> PartySim<S> {
         }
     }
 
+    /// Retrieves state machine output
+    ///
+    /// State machine **must be** in the output state. If state machine is in other state (e.g.
+    /// waits for an incoming message), this function will panic.
     #[track_caller]
     pub fn outputs(&mut self) -> Expect<S::Output> {
         loop {
@@ -61,10 +74,14 @@ impl<S: round_based::state_machine::StateMachine> PartySim<S> {
     }
 }
 
+/// Wraps `T` and allows to make assertions on it
 #[must_use = "you need to make sure the output meets tests expectations"]
 pub struct Expect<T>(pub T);
 
 impl<T: Eq + core::fmt::Debug> Expect<T> {
+    /// Wrapped value must be equal to `expected`
+    ///
+    /// Panics if it's not
     #[track_caller]
     pub fn expect_eq(&self, expected: &T) {
         assert_eq!(self.0, *expected)
@@ -72,12 +89,14 @@ impl<T: Eq + core::fmt::Debug> Expect<T> {
 }
 
 impl<T, E: core::fmt::Debug> Expect<Result<T, E>> {
+    /// Unwraps a result
     #[track_caller]
     pub fn unwrap(self) -> Expect<T> {
         Expect(self.0.unwrap())
     }
 }
 impl<T: core::fmt::Debug, E> Expect<Result<T, E>> {
+    /// Unwraps an error from result
     #[track_caller]
     pub fn unwrap_err(self) -> Expect<E> {
         Expect(self.0.unwrap_err())
